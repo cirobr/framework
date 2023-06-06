6f3371623b8ae379a17df70ab34ac6897583203c
cirobr <ciro.rosa@alumni.usp.br>
2023-06-05 21:31:22 -0300

    split R install

--- linux/dev.sh
+++ linux/dev.sh
@@ -9,12 +9,6 @@ sudo apt install git git-lfs
 sudo apt install build-essential gdb   # build-essential includes gcc g++ make
 sudo apt install mpich                 # OpenMPI
 
-# R
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
-sudo apt update
-sudo apt install r-base r-base-dev
-
 # other programming languages
 sudo apt install perl
 # sudo apt install gfortran
