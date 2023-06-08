2923f79edb795eeb26557f83faaba4e8a9ac01fd
cirobr <ciro.rosa@alumni.usp.br>
2023-06-08 16:36:22 -0300

    update 1.9.1

--- julia/install-julia.sh
+++ julia/install-julia.sh
@@ -2,6 +2,6 @@
 # https://julialang.org/downloads/platform/
 sudo apt install python3-pip
 pip install jill -U
-jill install 1.9.0
+jill install 1.9.1
 
 # sudo snap install julia --classic   # not suitable for VSCode
