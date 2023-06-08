14ce99af7951e866651506413ca08436dfa280fc
cirobr <ciro.rosa@alumni.usp.br>
2023-06-08 09:52:27 -0300

    new script

---
+++ docker/uninst.sh
@@ -0,0 +1,5 @@
+# https://docs.docker.com/engine/install/ubuntu/
+#!/bin/bash
+sudo apt remove docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt autoremove
+sudo rm /etc/apt/sources.list.d/docker*
