67fa36002c1450f8737b0da91e27cd893a93d32c
cirobr <ciro.rosa@alumni.usp.br>
2023-06-10 21:47:22 +0000

    new commit

---
+++ cuda-toolkit/install-nvidia-driver.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+clear
+sudo apt update
+sudo apt install nvidia-driver-530
+# sudo apt install nvidia-driver-530-open
---
+++ cuda-toolkit/uninst-nvidia-driver.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+clear
+sudo apt update
+sudo apt remove nvidia-driver-530
+# sudo apt install nvidia-driver-530-open
+
+sudo apt autoremove
