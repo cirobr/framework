9b1f1d20efc2c1cd99d4fd28bc2aae660c1a41a1
cirobr <ciro.rosa@alumni.usp.br>
2023-06-11 17:16:12 -0300

    code cleanup

--- miniconda/install.sh
+++ miniconda/install.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
+### https://repo.anaconda.com/miniconda/
 clear
 
 # get architecture
 if [ "$(uname -m)" = "x86_64" ]; then
     arch="x86_64"
-    echo $arch
 elif [ "$(uname -m)" = "aarch64" ]; then
     arch="arm64"
-    echo $arch
 else
     echo "wrong arch"
     exit 1
 fi
+
+echo $arch
 
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-$(uname)-$arch.sh
 bash ./Miniconda3-latest-$(uname)-$arch.sh
