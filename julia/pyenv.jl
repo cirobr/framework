f50a0121a4b5bd6677b5fa7c060112d022060d75
cirobr <ciro.rosa@alumni.usp.br>
2023-06-11 22:44:27 -0300

    wip

---
+++ julia/pyenv.jl
@@ -0,0 +1,21 @@
+# https://github.com/JuliaPy/PyCall.jl
+
+# setup environment
+condaenv = "pyenv"
+
+using Pkg
+using PyCall
+ENV["PYTHON"] = expanduser("~/miniconda3/envs/" * condaenv * "/bin/python3")
+# ENV["PYTHON"] = ""   # back to default conda/julia
+Pkg.build("PyCall")
+
+# check environment (may need to reboot julia)
+display(PyCall.pyversion)
+display(PyCall.libpython)
+PyCall.pyprogramname
+
+# import python libs
+# ros = pyimport("rosbag")
+# rs2 = pyimport("pyrealsense2")
+# bg  = pyimport("bagpy")
+# cvb = pyimport("cv_bridge")
