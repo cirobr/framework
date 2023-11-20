ea41d0b1ba176e3bd25c4495977653c1deb3b024
cirobr <ciro.rosa@alumni.usp.br>
2023-11-20 10:01:16 -0300

    new install

--- julia/cleanup-julia.sh
+++ julia/cleanup-julia.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # sudo snap remove julia
 rm -rf ~/.julia
-rm -rf ~/packages/julias
 rm ~/.local/bin/julia*
--- julia/install-julia.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# https://julialang.org/downloads/platform/
-# sudo apt install python3-pip
-pip install jill -U
-jill install 1.9.3
-
-# sudo snap install julia --classic   # not suitable for VSCode
