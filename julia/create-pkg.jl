120ff43ccf22ed2291369705153e54d0cbc45521
cirobr <ciro.rosa@alumni.usp.br>
2024-01-26 08:54:13 -0300

    update julia version to 1.10

--- julia/create-pkg.jl
+++ julia/create-pkg.jl
@@ -10,6 +10,6 @@ fullpath = path * lib
 # create package
 t=Template(user    = "cirobr",   # github username
            authors = "Ciro B Rosa <ciro.rosa@alumni.usp.br>", 
-           julia   = v"1.9",
+           julia   = v"1.10",
            dir     = path)
 t(lib)
