554a2ea9b7f0cac9ec3f6b990ad09f2180f88d3f
cirobr <ciro.rosa@alumni.usp.br>
2024-02-28 08:34:41 -0300

    update

--- julia/pkgs.jl
+++ julia/pkgs.jl
@@ -2,8 +2,8 @@ using Pkg
 Pkg.update()
 
 # development environment
-foldername = expanduser("~/juliaenvs/dev/")
-mkdir(foldername)
+foldername = expanduser("~/envs/dev/")
+mkpath(foldername)
 Pkg.activate(foldername)
 
 # jupyter interface
@@ -12,34 +12,21 @@ Pkg.add("IJulia")
 # private packages
 # https://github.com/GunnarFarneback/LocalRegistry.jl/blob/master/docs/ssh_keys.md
 Pkg.add(url="git@github.com:cirobr/PreprocessingImages.jl.git")
-Pkg.add(url="git@github.com:cirobr/UNetFlux.jl.git")
+Pkg.add(url="git@github.com:cirobr/TinyMachines.jl.git")
+Pkg.add(url="git@github.com:cirobr/LibML.jl.git")
+Pkg.add(url="git@github.com:cirobr/LibCUDA.jl.git")
+Pkg.add(url="git@github.com:cirobr/PascalVocTools.jl.git")
+
 
 # ai/ml
 Pkg.add("Flux")
-# Pkg.add("cuDNN")   # install only for GPU machines
 Pkg.add("MLJ")
-Pkg.add("MLJFlux")
 Pkg.add("MLUtils")
-Pkg.add("MLDatasets")   # mnist
-Pkg.add("MLJLinearModels")
-Pkg.add("MultivariateStats")
-Pkg.add("MLJMultivariateStatsInterface")
-Pkg.add("MLJScikitLearnInterface")
-Pkg.add("GLM")
-# Pkg.add("ScikitLearn")
-# Pkg.add("ObjectDetector")   # yolo
 
 # i/o
 Pkg.add("FileIO")
 Pkg.add("ImageIO")
 Pkg.add("VideoIO")
-
-# math
-Pkg.add("LinearAlgebra")
-Pkg.add("Metrics")
-Pkg.add("Random")
-Pkg.add("StatsBase")
-Pkg.add("Distributions")
 
 # charts
 Pkg.add("Plots")
@@ -49,22 +36,16 @@ Pkg.add("Printf")
 # media
 Pkg.add("Images")
 Pkg.add("Colors")
-Pkg.add("ImageDraw")
-# Pkg.add("ImageView")
-# Pkg.add("OpenCV")
-# Pkg.add("PerceptualColourMaps")   # depends on python matplotlib
-# Pkg.add("GLMakie")   # "using GLMakie" must come before "using VideoIO"
 
 # hpc
 Pkg.add("Distributed")
 Pkg.add("FLoops")
 
 # data
-Pkg.add("CSV")
 Pkg.add("DataFrames")
 Pkg.add("DataStructures")   # stacks, queues, ...
 Pkg.add("ArgParse")
-Pkg.add("BSON")
+Pkg.add("CSV")
 Pkg.add("JLD2")
 
 # tools
@@ -75,6 +56,7 @@ Pkg.add("PackageCompiler")
 
 # cuda
 # Pkg.add("CUDA")
+# Pkg.add("cuDNN")
 # using CUDA
 # if CUDA.has_cuda_gpu()   CUDA.versioninfo()   end
 
