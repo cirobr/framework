b281b896b029f14fdac2d938ea768a96b60532ea
cirobr <ciro.rosa@alumni.usp.br>
2024-04-03 08:28:12 -0300

    comment

--- julia/create-pkg.jl
+++ julia/create-pkg.jl
@@ -8,7 +8,7 @@ path     = expanduser("~/projects/")
 fullpath = path * lib
 
 # create package
-t=Template(user    = "cirobr",   # github username
+t=Template(user    = "cirobr",    # github username
            authors = "Ciro B Rosa <ciro.rosa@alumni.usp.br>", 
            julia   = v"1.10",
            dir     = path)
