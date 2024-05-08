e37f6ee5ecb8ec6859baf9e80af79b5a941b1aa1
cirobr <ciro.rosa@alumni.usp.br>
2024-05-08 10:11:58 -0300

    cleanup

--- keras/install-keras.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-clear
-# sudo apt update
-# sudo apt install pkg-config libhdf5-dev cmake
-
-conda create -n tf python numpy tensorflow keras matplotlib pandas scikit-learn ipykernel
-conda create -n tfgpu python numpy tensorflow-gpu keras matplotlib pandas scikit-learn ipykernel
