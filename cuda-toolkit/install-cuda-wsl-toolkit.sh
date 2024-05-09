fa25e8120c6fa06a5f806984f49f39754360403a
cirobr <ciro.rosa@alumni.usp.br>
2024-05-08 21:46:37 -0300

    update cudnn

--- cuda-toolkit/install-cuda-wsl-toolkit.sh
+++ cuda-toolkit/install-cuda-wsl-toolkit.sh
@@ -12,7 +12,6 @@ sudo apt-key del 7fa2af80
 
 # install new keys
 wget https://developer.download.nvidia.com/compute/cuda/repos/$distro/$arch/cuda-keyring_1.1-1_all.deb
-# wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
 sudo dpkg -i cuda-keyring_1.1-1_all.deb
 
 # install cuda toolkit
