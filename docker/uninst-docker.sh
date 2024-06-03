7c3949d397cea436c95ee845cfd742eee04748c9
cirobr <ciro.rosa@alumni.usp.br>
2024-06-03 16:04:23 -0300

    update

--- docker/uninst-docker.sh
+++ docker/uninst-docker.sh
@@ -3,3 +3,4 @@
 sudo apt remove docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo apt autoremove
 sudo rm /etc/apt/sources.list.d/docker*
+sudo rm /etc/apt/keyrings/docker.*
